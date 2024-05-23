c9f37bd2cfb334501b8fe3c7df6f26c70e94f7da
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-22 20:17:53 -0400

    Some updates for preproccess

--- src/health/prepare.py
+++ src/health/prepare.py
@@ -1,3 +1,17 @@
 from __future__ import division
 import nibabel
 import numpy as np
+import pkg
+import nibabel as nib
+import glob
+import os
+
+
+
+trainedData = []
+maskTrain = []
+maskaTrain = []
+FOVTrain = []
+
+
+idxCount = 1
